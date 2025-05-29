1d719c0dd5d7bb85f2c05655dc9b182ef53d47d5
= <jancernik12@gmail.com>
2025-05-29 14:34:16 -0300

    ThemeToggler component

---
+++ client/src/components/common/ThemeToggler.vue
@@ -0,0 +1,16 @@
+<script setup>
+import RButton from '@/components/RButton.vue'
+import { useTheme } from '@/composables/useTheme'
+
+const { setTheme, themeClass } = useTheme()
+
+const toggleTheme = () => {
+  setTheme(themeClass.value === 'dark' ? 'light' : 'dark')
+}
+</script>
+
+<template>
+  <RButton :icon="themeClass === 'dark' ? 'Sun' : 'Moon'" @click="toggleTheme"> </RButton>
+</template>
+
+<style lang="scss" scoped></style>
