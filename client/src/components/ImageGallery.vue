2d7df3c25a15047fe33852e1463072bdf108e9f7
= <jancernik12@gmail.com>
2025-07-25 13:35:47 -0300

    Remove the complex GSAP timelines in the image gallery

--- client/src/components/ImageGallery.vue
+++ client/src/components/ImageGallery.vue
@@ -7,16 +7,6 @@ import { ScrollSmoother } from "gsap/ScrollSmoother"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
 import { computed, nextTick, onMounted, onUnmounted, ref, useTemplateRef, watch } from "vue"
 
-const CENTER_DURATION = 1 // seconds
-const ENTER_AND_EXIT_DURATION = 2 // seconds
-const ENTER_AND_EXIT_INITIAL_DURATION = 1 // seconds
-const ENTER_AND_EXIT_SCALE = 0.7 // scale factor
-const TRANSFORM_ORIGIN_SCALE = -0.5 // scale factor
-const ENTER_AND_EXIT_OPACITY = 1 // opacity factor
-const OFFSET = 100 // pixels
-const START = 100 // percent
-const END = 0 // percent
-const SCRUB = 1 // seconds
 const BASE_LAG = 0.5 // seconds
 const LAG_SCALE = 0.5 // seconds
 const SMOOTH = 1 // seconds
@@ -26,7 +16,6 @@ const COLUMNS = 7 // number of columns
 const imageData = ref([])
 const groupedImages = ref([])
 const smoother = ref(null)
-const timelines = []
 const columnScrollTriggers = []
 const loadedImageIds = ref(new Set())
 
@@ -118,88 +107,11 @@ const setupLagEffect = () => {
   })
 }
 
-const getHorizontalOrigin = (element) => {
-  const rect = element.getBoundingClientRect()
-  const windowCenter = window.innerWidth / 2
-  const elementCenter = rect.left + element.offsetWidth / 2
-  const distanceFromScreenCenter = elementCenter - windowCenter
-  const scaledDistance = distanceFromScreenCenter * TRANSFORM_ORIGIN_SCALE
-  return element.offsetWidth / 2 - scaledDistance
-}
-
-const cleanupTimelines = () => {
-  timelines.forEach((timeline) => timeline.kill())
-  timelines.length = 0
-}
-
-const setupTimelines = () => {
-  cleanupTimelines()
-
-  const columns = imageGallery.value.querySelectorAll(".gallery-column")
-
-  columns.forEach((column) => {
-    const imageCards = column.querySelectorAll(".image-card")
-
-    imageCards.forEach((card) => {
-      const timeline = gsap.timeline({
-        defaults: {
-          ease: "power1.inOut"
-        },
-        scrollTrigger: {
-          end: () => `${-OFFSET + card.offsetHeight / 2}px ${END}%`,
-          scrub: SCRUB,
-          start: () => `${-OFFSET + card.offsetHeight / 2}px ${START}%`,
-          trigger: card
-        }
-      })
-
-      const yOriginTop = 0
-      const yOriginBottom = card.offsetHeight
-      const yOriginCenter = card.offsetHeight / 2
-      const xOrigin = getHorizontalOrigin(card)
-      timeline
-        .from(card, {
-          duration: ENTER_AND_EXIT_DURATION,
-          scale: ENTER_AND_EXIT_SCALE,
-          transformOrigin: () => `${xOrigin}px ${yOriginBottom}px`,
-          y: OFFSET
-        })
-        .from(
-          card,
-          {
-            duration: ENTER_AND_EXIT_INITIAL_DURATION,
-            opacity: ENTER_AND_EXIT_OPACITY,
-            transformOrigin: () => `${xOrigin}px ${yOriginBottom}px`
-          },
-          "<"
-        )
-        .to(card, {
-          duration: CENTER_DURATION,
-          opacity: 1,
-          scale: 1,
-          transformOrigin: () => `${xOrigin}px ${yOriginCenter}px`,
-          y: 0
-        })
-        .to(card, {
-          duration: ENTER_AND_EXIT_DURATION,
-          scale: ENTER_AND_EXIT_SCALE,
-          transformOrigin: () => `${xOrigin}px ${yOriginTop}px`,
-          y: -OFFSET
-        })
-
-      timelines.push(timeline)
-    })
-  })
-}
-
 const handleImageLoad = (imageId) => {
   loadedImageIds.value.add(imageId)
 
   if (loadedImageIds.value.size === imageData.value.length) {
-    nextTick(() => {
-      setupLagEffect()
-      setupTimelines()
-    })
+    nextTick(setupLagEffect)
   }
 }
 
@@ -218,7 +130,6 @@ onMounted(async () => {
 
 onUnmounted(() => {
   smoother.value?.kill()
-  cleanupTimelines()
 })
 </script>
 
