b2157c8c39da46194437744304bee74d92ff479e
Jan Cernik <jancernik12@gmail.com>
2025-10-28 23:49:11 -0300

    Add tiny images fallback for thumbnails

--- client/src/components/images/ImageCard.vue
+++ client/src/components/images/ImageCard.vue
@@ -17,9 +17,12 @@ const props = defineProps({
   }
 })
 
-const emit = defineEmits(["load", "click"])
+const emit = defineEmits(["load", "click", "tinyLoad"])
 
 const errorLoading = ref(false)
+const tinyLoaded = ref(false)
+const thumbnailLoaded = ref(false)
+const tiny = getImageVersion(props.image, "tiny")
 const thumbnail = getImageVersion(props.image, "thumbnail")
 
 const handleClick = (event) => {
@@ -28,6 +31,16 @@ const handleClick = (event) => {
     return
   }
   emit("click", event, props.image, `img-${props.image.id}`)
+}
+
+const handleTinyLoad = () => {
+  tinyLoaded.value = true
+  emit("tinyLoad", props.image.id)
+}
+
+const handleThumbnailLoad = () => {
+  thumbnailLoaded.value = true
+  emit("load", props.image.id)
 }
 </script>
 
@@ -41,14 +54,27 @@ const handleClick = (event) => {
     @touchend="handleClick"
   >
     <img
+      v-show="shouldLoad"
+      class="thumbnail"
+      :class="{ loaded: thumbnailLoaded }"
       :src="shouldLoad ? thumbnail.path : null"
       :height="thumbnail.height"
       :width="thumbnail.width"
       :title="image.captions?.en"
       :alt="image.captions?.en"
       :data-id="image.id"
-      @load="emit('load', image.id)"
-      @error="errorLoading = shouldLoad ? true : false"
+      @load="handleThumbnailLoad"
+      @error="errorLoading = true"
+    />
+
+    <img
+      class="tiny-fallback"
+      :class="{ loaded: tinyLoaded, hidden: thumbnailLoaded }"
+      :src="tiny.path"
+      :height="thumbnail.height"
+      :width="thumbnail.width"
+      :alt="image.captions?.en"
+      @load="handleTinyLoad"
     />
   </div>
 </template>
@@ -65,6 +91,7 @@ const handleClick = (event) => {
   position: absolute;
   width: inherit;
   user-select: none;
+  overflow: hidden;
   @include light-dark-property(background-color, rgba(#171717, 0.05), rgba(#e5e5e5, 0.05));
   transition: outline-offset 0.1s ease-in-out;
 
@@ -109,7 +136,7 @@ const handleClick = (event) => {
     }
   }
 
-  &.not-loaded img {
+  &.not-loaded .thumbnail {
     opacity: 0;
   }
 
@@ -123,6 +150,47 @@ const handleClick = (event) => {
 
     &:-moz-loading {
       visibility: hidden;
+    }
+  }
+
+  .tiny-fallback {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    filter: blur(15px);
+    opacity: 0;
+    transform: scale(1.05);
+    transition: opacity 0.3s ease-out;
+    pointer-events: none;
+
+    &.loaded {
+      opacity: 1;
+      visibility: visible;
+    }
+
+    &.hidden {
+      opacity: 0;
+      visibility: hidden;
+      transition:
+        opacity 0.4s ease-out,
+        visibility 0.4s ease-out;
+    }
+  }
+
+  &[style*="visibility: hidden"] .tiny-fallback {
+    opacity: 0;
+    visibility: hidden;
+  }
+
+  .thumbnail {
+    position: relative;
+    z-index: 2;
+    opacity: 0;
+    transition: opacity 0.4s ease-out;
+
+    &.loaded {
+      opacity: 1;
     }
   }
 }
