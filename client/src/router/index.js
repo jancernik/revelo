d3834d616c114888a1ed9fab0c7e29382db3c1e8
Jan Cernik <jancernik12@gmail.com>
2025-11-14 20:19:31 -0300

    Exclude the /dev route for production

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -23,94 +23,97 @@ import VerificationPending from "#src/views/VerificationPending.vue"
 import VerifyEmail from "#src/views/VerifyEmail.vue"
 import { createRouter, createWebHistory } from "vue-router"
 
+const routes = [
+  { component: Home, name: "home", path: "/" },
+  { component: Login, name: "login", path: "/login" },
+  { component: Signup, name: "signup", path: "/signup" },
+  { component: VerificationPending, name: "verification-pending", path: "/verification-pending" },
+  { component: VerifyEmail, name: "verify-email", path: "/verify-email" },
+  {
+    children: [
+      { component: DashboardIndex, name: "dashboard", path: "" },
+      { component: UploadImages, name: "upload", path: "upload" },
+      { component: DashboardSettings, name: "settings", path: "settings" },
+      { component: DashboardTasks, name: "tasks", path: "tasks" },
+      {
+        children: [
+          {
+            component: Collections,
+            name: "collections-list",
+            path: ""
+          },
+          {
+            component: CreateOrEditCollection,
+            name: "collection-new",
+            path: "new"
+          },
+          {
+            component: CreateOrEditCollection,
+            name: "collection-edit",
+            path: ":id/edit",
+            props: true
+          },
+          {
+            component: Collection,
+            name: "collection-view",
+            path: ":id",
+            props: true
+          },
+          {
+            component: SelectCollectionImages,
+            name: "collection-images",
+            path: ":id/images",
+            props: true
+          }
+        ],
+        component: CollectionsLayout,
+        name: "collections",
+        path: "collections"
+      },
+      {
+        children: [
+          {
+            component: Images,
+            name: "images-list",
+            path: ""
+          },
+          {
+            component: EditImage,
+            name: "image-edit",
+            path: ":id/edit",
+            props: true
+          },
+          {
+            component: Image,
+            name: "image-view",
+            path: ":id",
+            props: true
+          },
+          {
+            component: UploadImages,
+            name: "image-upload",
+            path: "upload"
+          }
+        ],
+        component: ImagesLayout,
+        name: "images",
+        path: "images"
+      }
+    ],
+    component: DashboardLayout,
+    meta: { requiresAuth: true },
+    path: "/dashboard"
+  },
+
+  { component: ImageDisplay, name: "public-image-view", path: "/images/:id", props: true },
+  { component: DevIndex, name: "dev", path: "/dev", production: false },
+  { component: NotFound, path: "/:catchAll(.*)" }
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    { component: Home, name: "home", path: "/" },
-    { component: Login, name: "login", path: "/login" },
-    { component: Signup, name: "signup", path: "/signup" },
-    { component: VerificationPending, name: "verification-pending", path: "/verification-pending" },
-    { component: VerifyEmail, name: "verify-email", path: "/verify-email" },
-    {
-      children: [
-        { component: DashboardIndex, name: "dashboard", path: "" },
-        { component: UploadImages, name: "upload", path: "upload" },
-        { component: DashboardSettings, name: "settings", path: "settings" },
-        { component: DashboardTasks, name: "tasks", path: "tasks" },
-        {
-          children: [
-            {
-              component: Collections,
-              name: "collections-list",
-              path: ""
-            },
-            {
-              component: CreateOrEditCollection,
-              name: "collection-new",
-              path: "new"
-            },
-            {
-              component: CreateOrEditCollection,
-              name: "collection-edit",
-              path: ":id/edit",
-              props: true
-            },
-            {
-              component: Collection,
-              name: "collection-view",
-              path: ":id",
-              props: true
-            },
-            {
-              component: SelectCollectionImages,
-              name: "collection-images",
-              path: ":id/images",
-              props: true
-            }
-          ],
-          component: CollectionsLayout,
-          name: "collections",
-          path: "collections"
-        },
-        {
-          children: [
-            {
-              component: Images,
-              name: "images-list",
-              path: ""
-            },
-            {
-              component: EditImage,
-              name: "image-edit",
-              path: ":id/edit",
-              props: true
-            },
-            {
-              component: Image,
-              name: "image-view",
-              path: ":id",
-              props: true
-            },
-            {
-              component: UploadImages,
-              name: "image-upload",
-              path: "upload"
-            }
-          ],
-          component: ImagesLayout,
-          name: "images",
-          path: "images"
-        }
-      ],
-      component: DashboardLayout,
-      meta: { requiresAuth: true },
-      path: "/dashboard"
-    },
-
-    { component: ImageDisplay, name: "public-image-view", path: "/images/:id", props: true },
-    { component: DevIndex, name: "dev", path: "/dev" },
-    { component: NotFound, path: "/:catchAll(.*)" }
-  ]
+  routes:
+    import.meta.env.PROD === true ? routes.filter((route) => route.production !== false) : routes
 })
 
 router.beforeEach((to, _from, next) => {
