12375fa2c082f208265f57a341e1a6a3c7876eef
Jan Cernik <jancernik12@gmail.com>
2026-02-17 19:12:35 -0300

    Fix menu not being always hidden when using the url params

--- client/src/composables/useMenu.js
+++ client/src/composables/useMenu.js
@@ -5,10 +5,12 @@ const shouldAnimate = ref(false)
 
 export function useMenu() {
   const show = (animate = true) => {
+    if (isVisible.value) return
     shouldAnimate.value = animate
     isVisible.value = true
   }
   const hide = (animate = true) => {
+    if (!isVisible.value) return
     shouldAnimate.value = animate
     isVisible.value = false
   }
